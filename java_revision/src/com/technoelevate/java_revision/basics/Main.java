2a2f1e9cbca294e2e68b283b8871d8a9b76413df
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:07:28 +0530

    Create Main.java

---
+++ java_revision/src/com/technoelevate/java_revision/basics/Main.java
@@ -0,0 +1,12 @@
+package com.technoelevate.java_revision.basics;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Abstraction abstraction=new Abstraction(2,2,12);
+		System.out.println(abstraction.getI());
+		System.out.println(abstraction.getJ());
+		System.out.println(abstraction.getK());
+	}
+
+}
