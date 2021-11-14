8ae5dd6f09f981cfe6a015b6b4a47bd2db61a2cc
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 08:46:21 +0530

    Create AbstractionMain.java

---
+++ abstraction/src/com/technoelevate/javabasics/abstraction/AbstractionMain.java
@@ -0,0 +1,25 @@
+package com.technoelevate.javabasics.abstraction;
+import java.util.Scanner;
+
+public class AbstractionMain {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan=new Scanner(System.in);
+		System.out.println("select the option for browser as per given below");
+		System.out.println("1.chrome\n2.operamini\n3.ucbrowser");
+		@SuppressWarnings("unused")
+		int ch =scan.nextInt();
+		@SuppressWarnings("unused")
+		int chrome = 0;
+//		if( chrome==1){
+//			chrome.start();
+//		}
+		int operamini = 0;
+		if(operamini==2) {
+			
+			
+		}
+	}
+
+}
