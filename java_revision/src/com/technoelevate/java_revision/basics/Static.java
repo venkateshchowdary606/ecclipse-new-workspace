6dca9e0a70663a37846196144692e3d1dab0b1d0
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:07:47 +0530

    Create Static.java

---
+++ java_revision/src/com/technoelevate/java_revision/basics/Static.java
@@ -0,0 +1,15 @@
+package com.technoelevate.java_revision.basics;
+
+public class Static {
+	static int a;
+	static int b;
+	static int c;
+	int d;
+	public static int sum(int a,int b) {
+		System.out.println("sum");
+		c=a+b;
+		return c;
+	}
+	
+
+}
