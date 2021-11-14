6ea163330e427f3efb6c388bf48bdfccb9b602f6
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:03:31 +0530

    Create UpcastTest.java

---
+++ mockpreparation/src/com/technoelevate/mockpreparation/UpcastTest.java
@@ -0,0 +1,24 @@
+package com.technoelevate.mockpreparation;
+
+import java.util.Scanner;
+
+public class UpcastTest {
+
+	public static void main(String[] args) {
+		
+int[] arr= {1,2,3};
+int[] arr1=new int[5];
+Scanner sc=new Scanner(System.in);
+    for(int i=0;i<arr1.length;i++) {
+    	arr1[i]=sc.nextInt();
+    	
+    for (int i1 : arr1) {
+    	
+    	System.out.println(i1);
+	 
+	}
+    
+	}
+
+	}
+	}
