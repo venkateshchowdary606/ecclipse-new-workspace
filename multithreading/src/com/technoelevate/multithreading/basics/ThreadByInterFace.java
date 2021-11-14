939d90572aa83c753edbe392fc5f6e7e42dc2e9a
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:05:59 +0530

    Create ThreadByInterFace.java

---
+++ multithreading/src/com/technoelevate/multithreading/basics/ThreadByInterFace.java
@@ -0,0 +1,15 @@
+package com.technoelevate.multithreading.basics;
+
+public class ThreadByInterFace  implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 15; i++) {
+			System.out.println("hello========");
+			
+		}
+		
+	}
+
+}
