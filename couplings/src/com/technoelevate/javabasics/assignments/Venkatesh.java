8912a40344a75f0d2c47995300e58422061ca8d7
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:12:30 +0530

    Create Venkatesh.java

---
+++ couplings/src/com/technoelevate/javabasics/assignments/Venkatesh.java
@@ -0,0 +1,8 @@
+package com.technoelevate.javabasics.assignments;
+
+public class Venkatesh implements Collaegues {
+	@Override
+	public void type(){
+		System.out.println("Projector Manager");
+	}
+}
