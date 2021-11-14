2ac2a9285aa97ad93c67fca820d9bc018758e702
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:07:55 +0530

    Create StringDemo.java

---
+++ java_revision/src/com/technoelevate/java_revision/basics/StringDemo.java
@@ -0,0 +1,20 @@
+package com.technoelevate.java_revision.basics;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s1="jAva";
+		String s2= "java";
+//		System.out.println(s1);
+//		System.out.println(s2);
+//		System.out.println(s1.concat(s2));
+		if(s1.equalsIgnoreCase(s2)) {
+			System.out.println("steve smith");
+		}
+		else {
+			System.out.println("samara");
+		}
+
+	}
+
+}
