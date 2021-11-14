ae9b76a8c2d52062f1615ffdd2020cf8059d9dba
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:03:24 +0530

    Create Practice1.java

---
+++ mockpreparation/src/com/technoelevate/mockpreparation/Practice1.java
@@ -0,0 +1,19 @@
+package com.technoelevate.mockpreparation;
+
+import java.util.Scanner;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		
+	
+	
+		
+	        Scanner scan = new Scanner(System.in);
+	        //Complete the code here
+	        String S=scan.nextLine();
+	        for(int i=0;i<S.length();i++){
+	            System.out.println(S.charAt(i));
+	        }
+	    }
+}
+
