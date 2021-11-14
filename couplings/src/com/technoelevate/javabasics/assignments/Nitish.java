7a48d4c22a45f2f8592e2eab307118c4f886c938
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:12:20 +0530

    Create Nitish.java

---
+++ couplings/src/com/technoelevate/javabasics/assignments/Nitish.java
@@ -0,0 +1,9 @@
+package com.technoelevate.javabasics.assignments;
+
+public class Nitish implements Collaegues {
+	@Override
+	public void type(){
+		System.out.println("I Belive,Energy Flows");
+	}
+
+}
