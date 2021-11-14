b3347dfec0ea723621e0f05a94483a9a2691ec14
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 08:51:53 +0530

    Create Main.java

---
+++ challange/src/com/technoelevate/challange/Main.java
@@ -0,0 +1,10 @@
+package com.technoelevate.challange;
+
+public class Main {
+
+	public static void main(String[] args) {
+  Solution s=new Solution("Electronics", 2500, 125);
+         s.display();
+	}
+
+}
