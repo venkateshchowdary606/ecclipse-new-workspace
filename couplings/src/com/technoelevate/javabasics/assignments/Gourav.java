a861302bcd25cc66a504f6f98a8ed2946a679e6a
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:12:11 +0530

    Create Gourav.java

---
+++ couplings/src/com/technoelevate/javabasics/assignments/Gourav.java
@@ -0,0 +1,10 @@
+package com.technoelevate.javabasics.assignments;
+
+public class Gourav implements Collaegues {
+
+	@Override
+	public void type() {
+		System.out.println("Number One BSD guy");		
+	}
+
+}
