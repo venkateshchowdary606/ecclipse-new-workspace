d8809c902f9ac6f6318d3411e01f0064c035dc11
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 08:46:41 +0530

    Create OperaMini.java

---
+++ abstraction/src/com/technoelevate/javabasics/abstraction/OperaMini.java
@@ -0,0 +1,11 @@
+package com.technoelevate.javabasics.abstraction;
+
+public class OperaMini implements Browser {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+System.out.println("opera opens");
+	}
+
+}
