8f2b5a44169a44a13f24478fb3a61bf66f227400
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:04:34 +0530

    Create Freshlite1.java

---
+++ multithreading/src/com/technoelevate/multithreading/assignments/Freshlite1.java
@@ -0,0 +1,12 @@
+package com.technoelevate.multithreading.assignments;
+
+public class Freshlite1 implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("welecome to test yantra");
+		
+		System.out.println();
+	}
+
+}
