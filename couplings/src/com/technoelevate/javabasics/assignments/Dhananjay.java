9c270ea4b3651ae7af29621af462323e7f2bf269
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:11:58 +0530

    Create Dhananjay.java

---
+++ couplings/src/com/technoelevate/javabasics/assignments/Dhananjay.java
@@ -0,0 +1,9 @@
+package com.technoelevate.javabasics.assignments;
+
+public class Dhananjay implements Collaegues{
+	@Override
+	public void type(){
+		System.out.println("Project Leader");
+	}
+
+}
