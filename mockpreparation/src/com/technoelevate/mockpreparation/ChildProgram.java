51c039580e6d5feca1ef6bdf8405b50d85c35e74
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:03:15 +0530

    Create ChildProgram.java

---
+++ mockpreparation/src/com/technoelevate/mockpreparation/ChildProgram.java
@@ -0,0 +1,13 @@
+package com.technoelevate.mockpreparation;
+
+public class ChildProgram extends CastingProgram{
+
+	public ChildProgram(int a, int b) {
+		super(a, b);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+
+}
