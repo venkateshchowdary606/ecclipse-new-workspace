ccf44a8c427855c01319457a815f7d26da9df391
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:07:43 +0530

    Create StaticMain.java

---
+++ java_revision/src/com/technoelevate/java_revision/basics/StaticMain.java
@@ -0,0 +1,12 @@
+package com.technoelevate.java_revision.basics;
+
+public class StaticMain {
+
+	public static void main(String[] args) {
+       Static.sum(10,10);
+       Static.a=10;
+       Static.b=10;
+       System.out.println(Static.c);
+	}
+
+}
