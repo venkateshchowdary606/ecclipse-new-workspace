69677e5fffd3fc1b5f370730c63239429108cee6
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:07:21 +0530

    Create Brain.java

---
+++ java_revision/src/com/technoelevate/java_revision/basics/Brain.java
@@ -0,0 +1,25 @@
+package com.technoelevate.java_revision.basics;
+
+public class Brain {
+	private double weight;
+	private int size;
+	public Brain(double weight, int size) {
+		super();
+		this.weight = weight;
+		this.size = size;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+
+}
