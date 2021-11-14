67e182217cd8443af7d03a4f3bd7e9a832b7298d
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 08:50:51 +0530

    Create MyArrayListMain.java

---
+++ collections/src/com/technoelevate/collections/arraylist/MyArrayListMain.java
@@ -0,0 +1,13 @@
+package com.technoelevate.collections.arraylist;
+
+public class MyArrayListMain {
+	public static void main(String[] args) {
+	
+		ArrayListCreation arrayListCreation = new ArrayListCreation(5);
+		arrayListCreation.add(10);
+		arrayListCreation.add(20);
+		arrayListCreation.add(82);
+		System.out.println(arrayListCreation);
+		
+	}
+}
