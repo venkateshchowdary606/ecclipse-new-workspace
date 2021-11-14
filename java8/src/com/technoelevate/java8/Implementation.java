26243dba0eb712b5cda57905b6dc08cbf17963b7
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:10:47 +0530

    Create Implementation.java

---
+++ java8/src/com/technoelevate/java8/Implementation.java
@@ -0,0 +1,16 @@
+package com.technoelevate.java8;
+
+public class Implementation {
+
+	public static void main(String[] args) {
+    @SuppressWarnings("unused")
+	AnonymousClass anonymousClass=new AnonymousClass() {
+    	@SuppressWarnings("unused")
+		public void name() {
+    		System.out.println("hello");
+    	}
+	};
+
+	}
+
+}
