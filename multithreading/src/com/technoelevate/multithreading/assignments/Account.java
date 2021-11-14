2f241a0d8c3aa8dad9ab2bca5cc8649047c471c7
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:04:29 +0530

    Create Account.java

---
+++ multithreading/src/com/technoelevate/multithreading/assignments/Account.java
@@ -0,0 +1,36 @@
+package com.technoelevate.multithreading.assignments;
+
+public class Account {
+	int balance;
+
+	public Account(int balance) {
+		this.balance = balance;
+	}
+	public synchronized void withdraw(int amount) {
+		if(amount>balance) {
+			System.out.println("insufficient balance");
+		}
+		else {
+			balance=balance-amount;
+			System.out.println("withdrawal sucessful");
+			System.out.println("balance="+balance);
+		}
+		
+		}
+		
+	
+public  void deposit(int amount) {
+	int temp =balance;
+	temp=temp+amount;
+	balance=temp;
+	System.out.println("deposit sucessful");
+	System.out.println("balance="+balance);
+	
+}
+public void checkBalance() {
+	System.out.println("balance="+balance);
+	
+}
+}
+
+
