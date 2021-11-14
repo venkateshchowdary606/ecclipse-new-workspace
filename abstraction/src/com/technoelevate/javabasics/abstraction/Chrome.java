1055cfc3b7973aeb3dcd8812d8c3ac665fd280f4
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 08:46:33 +0530

    Create Chrome.java

---
+++ abstraction/src/com/technoelevate/javabasics/abstraction/Chrome.java
@@ -0,0 +1,11 @@
+package com.technoelevate.javabasics.abstraction;
+
+public class Chrome implements Browser {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+System.out.println("chrome opens");
+	}
+
+}
