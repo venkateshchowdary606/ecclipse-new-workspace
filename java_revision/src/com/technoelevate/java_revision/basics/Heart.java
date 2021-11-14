9ade0b4a96fb4c6b07708983d83d954e6fac51ce
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:07:24 +0530

    Create Heart.java

---
+++ java_revision/src/com/technoelevate/java_revision/basics/Heart.java
@@ -0,0 +1,25 @@
+package com.technoelevate.java_revision.basics;
+
+public class Heart {
+	private double bp;
+	private double weight;
+	public Heart(double bp, double weight) {
+		super();
+		this.bp = bp;
+		this.weight = weight;
+	}
+	public double getBp() {
+		return bp;
+	}
+	public void setBp(double bp) {
+		this.bp = bp;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+
+}
