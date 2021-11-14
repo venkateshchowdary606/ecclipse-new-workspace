45c6c0a96bb13be26bb9834846c0991f8d3fb04d
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:07:08 +0530

    Create Abstraction.java

---
+++ java_revision/src/com/technoelevate/java_revision/basics/Abstraction.java
@@ -0,0 +1,33 @@
+package com.technoelevate.java_revision.basics;
+public class Abstraction {
+	private int i;
+	private int j;
+	private int k;
+	public Abstraction(int i, int j, int k) {
+		super();
+		this.i = i;
+		this.j = j;
+		this.k = k;
+	}
+	public int getI() {
+		
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	public int getK() {
+		return k;
+	}
+	public void setK(int k) {
+		this.k = k;
+	}
+	
+
+}
