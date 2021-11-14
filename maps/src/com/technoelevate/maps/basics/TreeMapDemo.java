43a5a5fcb2f5853939c75e7e9a059a629adefbff
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:03:47 +0530

    Create TreeMapDemo.java

---
+++ maps/src/com/technoelevate/maps/basics/TreeMapDemo.java
@@ -0,0 +1,10 @@
+package com.technoelevate.maps.basics;
+
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+	public static void main(String[] args) {
+		TreeMap<Integer, String> map=new TreeMap<Integer, String>();
+	}
+
+}
