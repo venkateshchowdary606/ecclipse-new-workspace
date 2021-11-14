f0265ae8fc03575548726b27a27604cc439a1ebd
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 08:46:37 +0530

    Create IcIc.java

---
+++ abstraction/src/com/technoelevate/javabasics/abstraction/IcIc.java
@@ -0,0 +1,8 @@
+package com.technoelevate.javabasics.abstraction;
+
+public class IcIc implements BhimUpi {
+		public void transfer(int amount) {
+		System.out.println("ICICI transfer money");
+		}
+		}
+		
