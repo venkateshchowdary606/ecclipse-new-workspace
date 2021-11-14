df08ecf0b0b1f72119e2a1bdd50b287a399a2b9c
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:01:18 +0530

    Create ComparatorMap.java

---
+++ maps/src/com/technoelevate/maps/basics/ComparatorMap.java
@@ -0,0 +1,13 @@
+package com.technoelevate.maps.basics;
+
+import java.util.Comparator;
+
+public abstract  class ComparatorMap implements Comparator<Student> {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
