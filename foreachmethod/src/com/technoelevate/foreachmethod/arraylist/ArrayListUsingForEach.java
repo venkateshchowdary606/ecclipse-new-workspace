ed977cb49193bf9c3250ae4c7b1fd6bfa75d1165
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:11:43 +0530

    Create ArrayListUsingForEach.java

---
+++ foreachmethod/src/com/technoelevate/foreachmethod/arraylist/ArrayListUsingForEach.java
@@ -0,0 +1,23 @@
+package com.technoelevate.foreachmethod.arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class ArrayListUsingForEach {
+	public static void main(String[] args) {
+		ArrayList<Integer> arrayList=new ArrayList<Integer>();
+		arrayList.add(10);
+		arrayList.add(15);
+		arrayList.add(18);
+		arrayList.add(17);
+		arrayList.add(18);
+		arrayList.add(12);
+		arrayList.forEach(l->{System.out.println(l);});
+       Arrays.sort		
+	
+	
+	}
+
+}
