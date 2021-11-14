a02c9c1d01cd33f9c400a0bbe343f8250e589882
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:03:27 +0530

    Create Test.java

---
+++ mockpreparation/src/com/technoelevate/mockpreparation/Test.java
@@ -0,0 +1,11 @@
+package com.technoelevate.mockpreparation;
+
+public class Test {
+
+	public static void main(String[] args) {
+	AbstractClass abstractClass=new AbstractMain();
+	abstractClass.sum();
+
+	}
+
+}
