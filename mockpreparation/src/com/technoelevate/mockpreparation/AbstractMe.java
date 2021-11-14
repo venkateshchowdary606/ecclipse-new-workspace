c71bb1b64d2ccdda087e72306ef8d21ed5504a2e
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:03:03 +0530

    Create AbstractMe.java

---
+++ mockpreparation/src/com/technoelevate/mockpreparation/AbstractMe.java
@@ -0,0 +1,12 @@
+package com.technoelevate.mockpreparation;
+
+abstract class AbstractMe {
+	static void  sum() {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void sub(){
+		System.out.println("sub");
+	}
+
+}
