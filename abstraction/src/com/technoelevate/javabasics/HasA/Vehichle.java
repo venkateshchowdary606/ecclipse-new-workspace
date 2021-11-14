1dabced2827dd1f42964b611c71da398ebf9aa01
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 08:47:09 +0530

    Create Vehichle.java

---
+++ abstraction/src/com/technoelevate/javabasics/HasA/Vehichle.java
@@ -0,0 +1,24 @@
+package com.technoelevate.javabasics.HasA;
+
+public class Vehichle {
+	String clr;
+	String price;
+	Engine engine;
+	Tyres tyres;
+	
+	public Vehichle(String clr, String price, Engine engine, Tyres tyres) {
+		super();
+		this.clr = clr;
+		this.price = price;
+		this.engine = engine;
+		this.tyres = tyres;
+	}
+	public void status() {
+		
+	}
+	}
+	
+	
+	
+
+
