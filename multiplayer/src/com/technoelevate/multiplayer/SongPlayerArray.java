5e2c7b4414a67d83472a01b16751758f5bc68f8a
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-14 09:04:14 +0530

    Create SongPlayerArray.java

---
+++ multiplayer/src/com/technoelevate/multiplayer/SongPlayerArray.java
@@ -0,0 +1,33 @@
+package com.technoelevate.multiplayer;
+
+import java.util.ArrayList;
+
+public class SongPlayerArray {
+	public static Object main;
+	public static void main(String[] args) {
+		
+		
+	
+	ArrayList<String> arrayList =new ArrayList<String>(8);
+	arrayList.add("1.NATU NATU");
+	arrayList.add("2.LEHARAYI");
+	arrayList.add("3.DOSTIII");
+	arrayList.add("4.BUTTA BOMMA");
+	arrayList.add("5.GEETHA GOVINDAM");
+	arrayList.add("6.JWALA REDDY");
+	arrayList.add("7.MANASA MANASA");
+	arrayList.add("8.ESWARA");
+	
+		System.out.println(arrayList);
+		ArrayList<String> song=arrayList;
+		
+		
+	}
+	public void display() {
+		System.out.println("songs are playing");
+		
+	}
+	
+	}
+
+
