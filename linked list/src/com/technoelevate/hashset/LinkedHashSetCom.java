dbae5015e340e8877a04793e34a69002f98caec1
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-15 09:11:37 +0530

    Create LinkedHashSetCom.java

---
+++ linked list/src/com/technoelevate/hashset/LinkedHashSetCom.java
@@ -0,0 +1,53 @@
+package com.technoelevate.hashset;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetCom implements Comparable<LinkedHashSetCom > {
+	int id;
+	String name;
+	
+
+
+public LinkedHashSetCom(String name,int id ) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+public static void main(String[] args) {
+	LinkedHashSet<LinkedHashSetCom> list=new LinkedHashSet<LinkedHashSetCom>();
+	LinkedHashSetComparator comparator=new LinkedHashSetComparator();
+list.add(new LinkedHashSetCom("venkatesh", 309));
+list.add(new LinkedHashSetCom("pranshu", 221));
+list.add(new LinkedHashSetCom("Vijay", 342));
+list.add(new LinkedHashSetCom("Avinash", 343));
+
+	ArrayList<LinkedHashSetCom>arrayList=new ArrayList<LinkedHashSetCom>(list);
+	Collections.sort(arrayList);
+	System.out.println(arrayList);
+	Collections.sort(arrayList, comparator);
+	System.out.println(arrayList);
+	 
+	}
+
+
+
+@Override
+public String toString() {
+	return "LinkedHashSetCom [id=" + id + ", name=" + name + "]";
+}
+
+@Override
+public int compareTo(LinkedHashSetCom o) {
+	// TODO Auto-generated method stub
+	return this.id-o.id;
+}
+
+
+}
+
+
+
