2d4950958e4aefec77945a5b342906f0c697ce87
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-15 09:11:44 +0530

    Create MainTree2.java

---
+++ treeset/src/com/technoelevate/assignment/treeset/MainTree2.java
@@ -0,0 +1,30 @@
+package com.technoelevate.assignment.treeset;
+
+public class MainTree2 implements Comparable<MainTree2>{
+	int rollNum;
+	String name;
+	
+
+	public MainTree2(int rollNum, String name) {
+		super();
+		this.rollNum = rollNum;
+		this.name = name;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "MainTree2 [rollNum=" + rollNum + ", name=" + name + "]";
+	}
+
+
+	@Override
+	public int compareTo(MainTree2 o) {
+		// TODO Auto-generated method stub
+		return this.rollNum-o.rollNum;
+	}
+
+}
+	
+
+
