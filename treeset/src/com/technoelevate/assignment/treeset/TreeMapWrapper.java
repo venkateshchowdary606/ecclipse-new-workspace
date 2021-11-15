ce0128965dc81ad1e9880bdf2e39585746d85458
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-15 09:12:13 +0530

    Create TreeMapWrapper.java

---
+++ treeset/src/com/technoelevate/assignment/treeset/TreeMapWrapper.java
@@ -0,0 +1,19 @@
+package com.technoelevate.assignment.treeset;
+
+import java.util.Comparator;
+
+
+public class TreeMapWrapper implements Comparator<MainTree>{
+
+	@Override
+	public int compare(MainTree o1, MainTree o2) {
+		// TODO Auto-generated method stub
+		return o2.rollNum-o1.rollNum;
+	}
+
+
+
+
+
+
+}
