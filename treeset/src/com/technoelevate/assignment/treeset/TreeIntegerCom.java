3a31719038f9b22861ec6dae2c2e8db75b39c90c
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-15 09:11:54 +0530

    Create TreeIntegerCom.java

---
+++ treeset/src/com/technoelevate/assignment/treeset/TreeIntegerCom.java
@@ -0,0 +1,16 @@
+package com.technoelevate.assignment.treeset;
+
+import java.util.Comparator;
+
+public class TreeIntegerCom implements Comparator<MainTree2>{
+
+	@Override
+	public int compare(MainTree2 o1, MainTree2 o2) {
+		// TODO Auto-generated method stub
+		return o2.rollNum-o1.rollNum;
+	}
+
+}
+
+
+
