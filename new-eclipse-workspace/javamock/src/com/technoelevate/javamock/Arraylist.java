1435c0356d025a75dbb5ad6f56969000a346e9d6
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-29 09:54:49 +0530

    Create Arraylist.java

---
+++ new-eclipse-workspace/javamock/src/com/technoelevate/javamock/Arraylist.java
@@ -0,0 +1,22 @@
+package com.technoelevate.javamock;
+
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+         int[]  arr= {1,2,5,4,8,3,};
+         for (int i = 0; i < arr.length; i++) {
+        	 for (int j = 0; j < arr.length; j++) {
+        		 if(arr[i]+arr[j]==6) {
+        			System.out.println("("+arr[i]+","+arr[j]+")");
+        			
+        			
+        			
+        		 }
+				
+			}
+			
+		}
+	}
+
+}
