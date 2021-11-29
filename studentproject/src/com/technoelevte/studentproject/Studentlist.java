4770bf7f74051139b9a22d30ff7e2145a665b4bb
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-29 09:54:55 +0530

    Create Studentlist.java

---
+++ studentproject/src/com/technoelevte/studentproject/Studentlist.java
@@ -0,0 +1,15 @@
+package com.technoelevte.studentproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Studentlist {
+
+	public static void main(String[] args) {
+       List<Student >list=new ArrayList<Student>();
+       list.add(new Student(315, "balla", 892, 21, 7799313094L));
+       list.add(new Student(315, "balla", 892, 21, 7799313094L));
+       list.add(new Student(315, "balla", 892, 21, 7799313094L));
+	}
+
+}
