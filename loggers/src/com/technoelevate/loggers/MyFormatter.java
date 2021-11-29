e48b2f5fd037ec1fee33420f63aa474a012c025e
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-29 09:53:30 +0530

    Create MyFormatter.java

---
+++ loggers/src/com/technoelevate/loggers/MyFormatter.java
@@ -0,0 +1,14 @@
+package com.technoelevate.loggers;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class MyFormatter extends Formatter {
+
+	@Override
+	public String format(LogRecord record) {
+		 
+		return record.getThreadID()+"";
+	}
+
+}
