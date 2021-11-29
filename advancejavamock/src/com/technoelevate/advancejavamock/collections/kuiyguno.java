a0ffa0671c01ea5f2ba2e288bf5c70e8836d8db0
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-29 10:45:39 +0530

    Create kuiyguno.java

---
+++ advancejavamock/src/com/technoelevate/advancejavamock/collections/kuiyguno.java
@@ -0,0 +1,13 @@
+package com.technoelevate.advancejavamock.collections;
+
+public class kuiyguno {
+	public static void main(String[] args) {
+		kuiyguno kuiyguno=new kuiyguno();
+		}
+	Runnable runnable=()->{
+		System.out.println("hello");
+	};
+		
+	}
+
+
