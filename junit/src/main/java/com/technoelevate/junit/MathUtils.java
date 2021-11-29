3bb49589744e641369f7aa23eb07d57498b3419b
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-29 10:44:12 +0530

    Create MathUtils.java

---
+++ junit/src/main/java/com/technoelevate/junit/MathUtils.java
@@ -0,0 +1,17 @@
+package com.technoelevate.junit;
+
+public class MathUtils {
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public double areaOfCircle(double r) {
+		return Math.PI * r * r;
+	}
+
+	public int divideTest(int a,int b) {
+		return  a/b;
+
+	}
+
+}
