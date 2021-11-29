720c8a3199be110d0fa38014268f43b0d204183d
venkateshchowdary606 <venkateshchowdary606@gmail.com>
2021-11-29 09:55:18 +0530

    Create NameClass.java

---
+++ studentproject/src/com/technoelevte/studentproject/NameClass.java
@@ -0,0 +1,11 @@
+package com.technoelevte.studentproject;
+
+public class NameClass implements Comparable<Student>{
+
+	@Override
+	public int compareTo(Student o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
